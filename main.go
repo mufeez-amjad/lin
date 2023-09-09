236d11a13df19e3c086bbcf45524dbb9e5aedb51
Mufeez Amjad <mufeez.amjad@outlook.com>
2023-09-09 18:18:04 -0400

    cleanup

--- main.go
+++ main.go
@@ -1,31 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"syscall"
-	"log"
-	"context"
 	"time"
-    "runtime"
-	
+
 	// Proto
-	"google.golang.org/grpc"
 	"lin_cli/proto"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
 	// Bubbles
-	bList "github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
+	bList "github.com/charmbracelet/bubbles/list"
 
 	// Tea
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 
 	// Git
-    "github.com/go-git/go-git/v5"
-    "github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
@@ -39,9 +41,9 @@ const (
 )
 
 type keyMap struct {
-	Enter 	key.Binding
-	C    	key.Binding
-	Quit  	key.Binding
+	Enter key.Binding
+	C     key.Binding
+	Quit  key.Binding
 }
 
 func (k keyMap) ShortHelp() []key.Binding {
@@ -66,9 +68,9 @@ var keys = keyMap{
 }
 
 type Issue struct {
-	identifier 	string
-	title 		string
-	data 		*proto.Issue
+	identifier string
+	title      string
+	data       *proto.Issue
 }
 
 func (i Issue) Title() string       { return i.identifier }
@@ -77,10 +79,10 @@ func (i Issue) Data() *proto.Issue  { return i.data }
 func (i Issue) FilterValue() string { return i.identifier + " " + i.title }
 
 type model struct {
-	list	bList.Model
-	help 	help.Model
-	keys 	keyMap
-	state 	sessionState
+	list  bList.Model
+	help  help.Model
+	keys  keyMap
+	state sessionState
 }
 
 func (m model) Init() tea.Cmd {
@@ -88,88 +90,88 @@ func (m model) Init() tea.Cmd {
 }
 
 func checkoutBranch(branchName string) error {
-    // Get the current working directory
-    cwd, err := os.Getwd()
-    if err != nil {
-        return fmt.Errorf("Error getting current working directory: %v\n", err)
-    }
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return fmt.Errorf("Error getting current working directory: %v\n", err)
+	}
 
-    r, err := git.PlainOpen(cwd)
-    if err != nil {
-        return fmt.Errorf("Error opening repository: %v\n", err)
-    }
+	r, err := git.PlainOpen(cwd)
+	if err != nil {
+		return fmt.Errorf("Error opening repository: %v\n", err)
+	}
 
-    refs, err := r.Branches()
-    if err != nil {
-        return fmt.Errorf("Error getting branches: %v\n", err)
-    }
+	refs, err := r.Branches()
+	if err != nil {
+		return fmt.Errorf("Error getting branches: %v\n", err)
+	}
 
-    branchExists := false
-    err = refs.ForEach(func(ref *plumbing.Reference) error {
-        if ref.Name().Short() == branchName {
-            branchExists = true
-        }
-        return nil
-    })
+	branchExists := false
+	err = refs.ForEach(func(ref *plumbing.Reference) error {
+		if ref.Name().Short() == branchName {
+			branchExists = true
+		}
+		return nil
+	})
 
-    if err != nil {
-        return fmt.Errorf("Error checking branch existence: %v\n", err)
-    }
+	if err != nil {
+		return fmt.Errorf("Error checking branch existence: %v\n", err)
+	}
 
-    // If the branch doesn't exist, create it
-    if !branchExists {
-        // Create a new branch from the current HEAD
-        headRef, err := r.Head()
-        if err != nil {
-            fmt.Printf("Error getting HEAD reference: %v\n", err)
-            os.Exit(1)
-        }
+	// If the branch doesn't exist, create it
+	if !branchExists {
+		// Create a new branch from the current HEAD
+		headRef, err := r.Head()
+		if err != nil {
+			fmt.Printf("Error getting HEAD reference: %v\n", err)
+			os.Exit(1)
+		}
 
-        newBranchRef := plumbing.NewBranchReferenceName(branchName)
-        branch := plumbing.NewHashReference(newBranchRef, headRef.Hash())
+		newBranchRef := plumbing.NewBranchReferenceName(branchName)
+		branch := plumbing.NewHashReference(newBranchRef, headRef.Hash())
 
-        if err := r.Storer.SetReference(branch); err != nil {
-            fmt.Printf("Error creating branch: %v\n", err)
-            os.Exit(1)
-        }
+		if err := r.Storer.SetReference(branch); err != nil {
+			fmt.Printf("Error creating branch: %v\n", err)
+			os.Exit(1)
+		}
 
-        fmt.Printf("Created branch '%s'\n", branchName)
-    }
+		fmt.Printf("Created branch '%s'\n", branchName)
+	}
 
-    // Checkout the branch
-    w, err := r.Worktree()
-    if err != nil {
-        fmt.Printf("Error getting worktree: %v\n", err)
-        os.Exit(1)
-    }
+	// Checkout the branch
+	w, err := r.Worktree()
+	if err != nil {
+		fmt.Printf("Error getting worktree: %v\n", err)
+		os.Exit(1)
+	}
 
-    err = w.Checkout(&git.CheckoutOptions{
-        Branch: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branchName)),
-        Keep: true,
-    })
-    if err != nil {
-        fmt.Printf("Error checking out branch: %v\n", err)
-        os.Exit(1)
-    }
+	err = w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(fmt.Sprintf("refs/heads/%s", branchName)),
+		Keep:   true,
+	})
+	if err != nil {
+		fmt.Printf("Error checking out branch: %v\n", err)
+		os.Exit(1)
+	}
 
-    fmt.Printf("Checked out branch '%s'\n", branchName)
-    return nil
+	fmt.Printf("Checked out branch '%s'\n", branchName)
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	issue := m.list.SelectedItem().(Issue).Data()
-	
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, m.keys.C):
 			// TODO: handle multiple branches (based on issue attachments)
 			branchName := issue.GetBranchName()
-            err := checkoutBranch(branchName)
-            if err != nil {
-                fmt.Printf("%s", err)
-            }
-            return m, tea.Quit
+			err := checkoutBranch(branchName)
+			if err != nil {
+				fmt.Printf("%s", err)
+			}
+			return m, tea.Quit
 		case key.Matches(msg, m.keys.Enter):
 			openURL(issue.GetUrl())
 		}
@@ -188,51 +190,52 @@ func (m model) View() string {
 }
 
 func openURL(href string) error {
-    var cmd *exec.Cmd
+	var cmd *exec.Cmd
 
 	// TODO: open desktop app instead of browser
 
-    // Determine the operating system and open the URL accordingly
-    switch runtime.GOOS {
-    case "darwin": // macOS
-        cmd = exec.Command("open", href)
-    case "windows":
-        cmd = exec.Command("cmd", "/c", "start", href)
-    case "linux":
-        cmd = exec.Command("xdg-open", href)
-    default:
-        return fmt.Errorf("unsupported platform")
-    }
+	// Determine the operating system and open the URL accordingly
+	switch runtime.GOOS {
+	case "darwin": // macOS
+		cmd = exec.Command("open", href)
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "start", href)
+	case "linux":
+		cmd = exec.Command("xdg-open", href)
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
 
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-    return cmd.Run()
+	return cmd.Run()
 }
 
 func spawnServer() (int, error) {
 	// Command to run the TypeScript server
-    cmd := exec.Command("node", "index.js")
+	cmd := exec.Command("node", "index.js")
 
-    // Set the current working directory to the directory containing the TypeScript server script
-    cmd.Dir = "server/dist"
+	// Set the current working directory to the directory containing the TypeScript server script
+	cmd.Dir = "server/dist"
 
 	// TODO: Remove after development
-    // Redirect standard output and error streams to the Go process's standard output and error streams
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+	// Redirect standard output and error streams to the Go process's standard output
+	// and error streams
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-    // Start the TypeScript server as a child process
-    if err := cmd.Start(); err != nil {
-        fmt.Printf("Error starting TypeScript server: %v\n", err)
-        return 0, err
-    }
+	// Start the TypeScript server as a child process
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("Error starting TypeScript server: %v\n", err)
+		return 0, err
+	}
 
 	// Capture the child process ID
 	childPid := cmd.Process.Pid
 
 	return childPid, nil
-}	
+}
 
 func main() {
 	// Spawn the TypeScript server
@@ -254,39 +257,50 @@ func main() {
 
 	// Connect to the TypeScript server
 	var conn *grpc.ClientConn
-    for {
-        var err error
-        conn, err = grpc.Dial("0.0.0.0:50051", grpc.WithInsecure())
-        if err != nil {
-            log.Printf("Failed to connect: %v. Retrying...", err)
-            time.Sleep(2 * time.Second) // Adjust the retry interval as needed
-            continue
-        }
-        break // Connection successful, exit the loop
-    }
+	for {
+		var err error
+		conn, err = grpc.Dial(
+			"0.0.0.0:50051",
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		)
+		if err != nil {
+			log.Printf("Failed to connect: %v. Retrying...", err)
+			time.Sleep(2 * time.Second) // Adjust the retry interval as needed
+			continue
+		}
+		break // Connection successful, exit the loop
+	}
 	defer conn.Close()
 	fmt.Println("Connected to server")
 
 	client := proto.NewLinearClient(conn)
 
 	req := &proto.GetIssuesRequest{
-		ApiKey: "lin_api_3nQjimQ7hLcGfMIX0pAiQkS3Li9cizR2WfL3wAui",
+		ApiKey: "",
 	}
 
-    resp, err := client.GetIssues(context.Background(), req)
-    if err != nil {
-        log.Fatalf("GetIssues failed: %v", err)
-    }
+	resp, err := client.GetIssues(context.Background(), req)
+	if err != nil {
+		log.Fatalf("GetIssues failed: %v", err)
+	}
 
 	// Do stuff in the CLI
 
 	items := []bList.Item{}
 
 	for _, issue := range resp.Issues {
-		items = append(items, Issue{identifier: issue.GetIdentifier(), title: issue.GetTitle(), data: issue})
+		items = append(items, Issue{
+			identifier: issue.GetIdentifier(),
+			title:      issue.GetTitle(),
+			data:       issue,
+		})
 	}
 
-	m := model{list: bList.New(items, bList.NewDefaultDelegate(), 0, 0), state: listView, keys: keys, help: help.New()}
+	m := model{
+		list:  bList.New(items, bList.NewDefaultDelegate(), 0, 0),
+		state: listView,
+		keys:  keys, help: help.New(),
+	}
 
 	m.list.AdditionalShortHelpKeys = func() []key.Binding {
 		return m.keys.ShortHelp()
@@ -300,10 +314,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	// syscall.Kill(childPid, syscall.SIGKILL)
-
-	// // Wait for the TypeScript server to exit (you can also implement logic to gracefully handle server exit)
-    // if err := cmd.Wait(); err != nil {
-    //     fmt.Printf("TypeScript server exited with error: %v\n", err)
-    // }
+	syscall.Kill(childPid, syscall.SIGKILL)
 }
