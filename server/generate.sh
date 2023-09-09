da8b5ae2bc50fb4e20f41afdb3f852778845eef9
Mufeez Amjad <mufeez.amjad@outlook.com>
2023-09-08 22:40:04 -0400

    generating go proto types

--- server/generate.sh
+++ server/generate.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
+cd "$(dirname "$0")" 
+
 OUT_DIR=./src/generated
+PROTO_DIR=../proto
 
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
 
+GRPC_TOOLS_PATH=$(npm root)/.bin/grpc_tools_node_protoc
+
 # generate js codes via grpc-tools
-grpc_tools_node_protoc \
+$GRPC_TOOLS_PATH \
 --js_out=import_style=commonjs,binary:${OUT_DIR} \
 --grpc_out=grpc_js:${OUT_DIR} \
 --plugin=protoc-gen-grpc=$(npm root)/.bin/grpc_tools_node_protoc_plugin \
--I ./proto \
-./proto/*.proto
+-I ${PROTO_DIR} \
+${PROTO_DIR}/*.proto
 
 # generate d.ts codes
 protoc \
 --plugin=protoc-gen-ts=$(npm root)/.bin/protoc-gen-ts \
 --ts_out=grpc_js:${OUT_DIR} \
--I ./proto \
-./proto/*.proto
+-I ${PROTO_DIR} \
+${PROTO_DIR}/*.proto
+
+cd - >/dev/null
